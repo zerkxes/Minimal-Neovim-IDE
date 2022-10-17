b5d3df8c6b9d411eda5c87aa14a876989812967d
Aniket Bose <aniketbose986@gmail.com>
2022-10-17 12:42:43 +0530

    Added Options

---
+++ lua/zerkxes/options.lua
@@ -0,0 +1,39 @@
+local options = {
+	backup=false,
+	clipboard="unnamedplus",
+	completeopt={"menuone","noselect"},
+	conceallevel=0,
+	fileencoding="utf-8",
+	hlsearch=true,
+	ignorecase=true,
+	mouse="a",
+	pumheight=10,
+	showtabline=2,
+	smartcase=true,
+	smartindent=true,
+	splitbelow=true,
+	splitright=true,
+	swapfile=false,
+	--termguicolors=true,   --do not touch this shit, looks horrible
+	timeoutlen=300,
+	undofile=true,
+	updatetime=300,
+	writebackup=false,
+	expandtab=true,
+	shiftwidth=2,
+	tabstop=2,
+	cursorline=true,
+	number=true,
+	relativenumber=true,
+	signcolumn="yes",
+	wrap=false,
+	scrolloff=8,
+	sidescrolloff=8,
+	guicursor="",
+}
+vim.opt.shortmess:append "c"
+for k,v in pairs(options) do
+	vim.opt[k]=v
+end
+vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd [[set iskeyword+=-]]
