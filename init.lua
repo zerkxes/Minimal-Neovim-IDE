79ede1064560840ea8ac75e0c2e23450ff429d71
Aniket Bose <aniketbose986@gmail.com>
2022-11-03 13:40:23 +0530

    ignore

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
 require "zerkxes.options"
 require "zerkxes.keymaps"
 vim.cmd "colorscheme evening"
+require "zerkxes.plugins"
+pcall(vim.cmd, 'colorscheme onedark')
+require "zerkxes.cmp"
+--require("mason").setup()
+--require "zerkxes.mason"
+--require("mason-lspconfig").setup()
+--require("lspconfig").jdtls.setup()
+--require'lspconfig'.clangd.setup{}
