a8409725c99d8d66a8a8a47bd362485c63367627
Aniket Bose <aniketbose986@gmail.com>
2022-11-22 13:19:00 +0530

    removed unecessary code

--- init.lua
+++ init.lua
@@ -1,11 +1,7 @@
 require "zerkxes.options"
 require "zerkxes.keymaps"
-vim.cmd "colorscheme evening"
 require "zerkxes.plugins"
 pcall(vim.cmd, 'colorscheme onedark')
 require "zerkxes.cmp"
 require "zerkxes.mason"
 require "zerkxes.lspconf"
---require("mason-lspconfig").setup()
---require("lspconfig").jdtls.setup()
---require'lspconfig'.clangd.setup{}
