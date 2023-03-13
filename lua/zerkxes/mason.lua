c853e90ce178cf2ee2c4b562acfbf0efd391547e
Aniket Bose <aniketbose986@gmail.com>
2023-03-13 18:58:24 +0530

    automatic installation of said lsps

--- lua/zerkxes/mason.lua
+++ lua/zerkxes/mason.lua
@@ -1,5 +1,7 @@
 require("mason").setup()
-require("mason-lspconfig").setup()
+require("mason-lspconfig").setup{
+  automatic_installation=true,
+}
 
 -- After setting up mason-lspconfig you may set up servers via lspconfig
 -- require("lspconfig").sumneko_lua.setup {}
